0724826d2e5769471c9e97f6f24463d0c6aee158
vtasun <tasun.v.s@gmail.com>
2025-02-26 22:34:26 +0200

    add one more sout

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Good Bye, World!");
+    System.out.println("Hello again, World!");
   }
 }
